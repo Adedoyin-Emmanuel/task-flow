11729043349446939c40f627142d623f5f464cb6
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-08-21 14:52:49 +0100

    feat: added about page

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,11 @@
 import "./App.css";
 import { BrowserRouter, Routes } from "react-router-dom";
-import login from "./pages/login";
-import signup from "./pages/signup";
-import projects from "./pages/projects";
-import tasks from "./pages/tasks";
-import dashboard from "./pages/dashboard";
-import reports from "./pages/reports";
-import profile from "./pages/profile";
-import me from "./pages/me";
+import about from "./pages/about";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-       
-  
-      </Routes>
+      <Routes></Routes>
     </BrowserRouter>
   );
 }
---
+++ src/pages/about/index.tsx
@@ -0,0 +1,17 @@
+
+import React from "react";
+import {Link, useNavigate} from "react-router-dom";
+
+const About = () => {
+
+    const navigateTo = useNavigate();
+
+    return (
+        <div>
+            <h1>About works!</h1>
+        </div>
+    );  
+}
+
+export default About;
+    
