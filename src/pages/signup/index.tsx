219579f690ac8daf121356b24062cca48a5f3975
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-08-21 17:07:34 +0100

    feat: finished signup page

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,8 +1,160 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Link } from "react-router-dom";
 
 const Signup = () => {
+  const formSchema = z.object({
+    fullname: z.string().min(2, {
+      message: "Fullname must be at least 2 characters.",
+    }),
+    email: z.string().email({ message: "Invalid email format." }),
+    password: z.string().min(6).max(20, {
+      message:
+        "Password must be at least 6 characters and at most 20 characters.",
+    }),
+    role: z.enum(["admin", "project manager", "team member"], {
+      message: "Invalid role specified",
+    }),
+  });
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      fullname: "",
+      email: "",
+      password: "",
+      role: "team member",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
   return (
-    <div>
-      <h1>Signup works!</h1>
+    <div className="flex items-center justify-center min-h-screen">
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="w-11/12 md:w-1/2 xl:w-1/4"
+        >
+          <h3 className="text-2xl capitalize font-bold">Signup</h3>
+          <p className="text-sm text-gray-600">
+            Create an account with Task Flow
+          </p>
+
+          <br />
+
+          <FormField
+            control={form.control}
+            name="fullname"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Fullname</FormLabel>
+                <FormControl>
+                  <Input placeholder="Enter your fullname" {...field} />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="Enter your email address"
+                    {...field}
+                    type="email"
+                  />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="Enter your password"
+                    {...field}
+                    type="password"
+                  />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="role"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Role</FormLabel>
+                <Select
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                >
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Select a role" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    <SelectItem value="team member">Team Member</SelectItem>
+                    <SelectItem value="project manager">
+                      Project Manager
+                    </SelectItem>
+                    <SelectItem value="admin">Admin</SelectItem>
+                  </SelectContent>
+                </Select>
+              </FormItem>
+            )}
+          />
+
+          <Button type="submit" className="w-full my-4">
+            Create Account
+          </Button>
+
+          <span className="text-sm text-gray-600">
+            Got an account?{" "}
+            <Link to="/auth/login" className="text-primary underline">
+              Login
+            </Link>
+          </span>
+        </form>
+      </Form>
     </div>
   );
 };
