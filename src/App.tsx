d4dfaedf3a1d0437bf05d72517c536a76278cdd3
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-08-21 14:41:15 +0100

    feat: added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,47 @@
-import { useState } from "react";
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <h2 className="text-4xl">Hello world!</h2>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<>Hello world</>} />
+        <Route path="/about" element={<></>} />
+
+        <Route path="/auth">
+          <Route path="/login" element={<>Login</>} />
+          <Route path="/signup" element={<>Signup</>} />
+        </Route>
+
+        <Route path="/admin">
+          <Route path="/users" element={<>Users page</>} />
+          <Route path="/users/:userId" element={<>User Details</>} />
+          <Route path="/reports"/>
+        </Route>  
+
+
+        <Route path="/manager">
+          <Route path="/projects" element={<>Projects</>} />
+          <Route path="/projects/:projectId" element={<>Project Details</>} />
+          <Route path="/projects/:projectId/edit" element={<>Project Details Edit</>} />
+          <Route path="/reports" />
+          <Route path="/tas"/>
+        </Route>
+
+        <Route path="/projects">
+          <Route path="/:projectId" element={<>Project Details</>} />
+          <Route path="/:projectId/edit" element={<>Project Details Edit</>} />
+          <Route path="/:projectId/edit" element={<>Project Details Edit</>} />
+        </Route>
+
+        <Route path="/dashboard">
+          <Route path="/profile" element={<>Profile</>} />
+          <Route path="/settings" element={<>Settings</>} />
+        </Route>
+
+        <Route path="*" element={<>Not Found</>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
