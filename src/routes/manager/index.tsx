8209b7bc5cc40f06f2a04d109056185657451dc4
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-08-21 16:21:38 +0100

    feat: added poroject manager routes

---
+++ src/routes/manager/index.tsx
@@ -0,0 +1,24 @@
+import { Route } from "react-router-dom";
+import Dashboard from "@/pages/dashboard";
+import Report from "@/pages/report";
+import UserId from "@/pages/userId";
+import Project from "@/pages/project";
+import ProjectId from "@/pages/project/projectId";
+import Task from "@/pages/task";
+import TaskId from "@/pages/task/taskId";
+
+export const projectManagerRoutes = [
+  <Route index path="" element={<Dashboard />} />,
+  <Route path="dashboard" element={<Dashboard />} key={"projectManagerDashboard"} />,
+  <Route path="reports" element={<Report />} key={"projectManagerReports"} />,
+  <Route path="users" element={<>all users</>} key={"projectManagerAllUsers"} />,
+  <Route path="users/:userId" element={<UserId />} key={"projectManagerUserId"} />,
+  <Route path="projects" element={<Project />} key={"projectManagerProjects"} />,
+  <Route
+    path="projects/:projectId"
+    element={<ProjectId />}
+    key={"projectManagerProjectId"}
+  />,
+  <Route path="tasks" element={<Task />} key={"projectManagerTasks"} />,
+  <Route path="tasks/:taskId" element={<TaskId />} key={"projectManagerTasks"} />,
+];
