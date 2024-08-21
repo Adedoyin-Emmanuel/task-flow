5523679757c92fb96e95ed17a8abb795ec595f2f
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-08-21 23:14:38 +0100

    feat: updated task page

--- src/pages/task/index.tsx
+++ src/pages/task/index.tsx
@@ -12,6 +12,18 @@ import {
 } from "@/components/ui/dropdown-menu";
 import TaskComponent from "@/components/task";
 import React from "react";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 interface ITask {
   className?: string;
@@ -19,6 +31,7 @@ interface ITask {
 
 const Task = ({ className }: ITask) => {
   const [statusFilter, setStatusFilter] = React.useState("all");
+  const [showCreateTaskDialog, setCreateTaskDialog] = React.useState(false);
 
   const tasks = [
     {
@@ -59,6 +72,41 @@ const Task = ({ className }: ITask) => {
     if (statusFilter === "all") return tasks;
     return task.status === statusFilter;
   });
+
+  const CreateTaskDialog = () => {
+    return (
+      <Dialog open={showCreateTaskDialog} onOpenChange={setCreateTaskDialog}>
+        <DialogTrigger asChild>
+          <Button variant="outline">Edit Profile</Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Edit profile</DialogTitle>
+            <DialogDescription>
+              Make changes to your profile here. Click save when you're done.
+            </DialogDescription>
+          </DialogHeader>
+          <div className="grid gap-4 py-4">
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="name" className="text-right">
+                Name
+              </Label>
+              <Input id="name" value="Pedro Duarte" className="col-span-3" />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="username" className="text-right">
+                Username
+              </Label>
+              <Input id="username" value="@peduarte" className="col-span-3" />
+            </div>
+          </div>
+          <DialogFooter>
+            <Button type="submit">Save changes</Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    );
+  };
 
   return (
     <div className={cn("", className)}>
@@ -111,11 +159,13 @@ const Task = ({ className }: ITask) => {
         </div>
       </Sidebar>
 
+      <CreateTaskDialog />
+
       <section className="fixed bottom-20 right-10">
         <section
           className={`w-16 h-16 flex items-center justify-center  bg-gray-50 rounded-full shadow cursor-pointer relative transform-gpu transition-transform duration-200 scale-100 hover:scale-110 ${className}`}
           onClick={() => {
-            console.log("clicked");
+            setCreateTaskDialog(true);
           }}
         >
           <FolderPlus className="h-10 w-10 " strokeWidth={1.2} />
