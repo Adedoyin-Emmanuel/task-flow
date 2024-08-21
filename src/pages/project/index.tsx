3a0f78128914eaeb15c7f8cd897a40883d84fa9e
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-08-21 22:25:05 +0100

    feat: updated project page

--- src/pages/project/index.tsx
+++ src/pages/project/index.tsx
@@ -1,5 +1,8 @@
 import { cn } from "@/lib/utils";
 import Sidebar from "@/components/sidebar";
+import StatsCard from "@/components/stats-card";
+import { Button } from "@/components/ui/button";
+import { FolderPlus } from "lucide-react";
 
 interface IProject {
   className?: string;
@@ -69,7 +72,28 @@ const Project = ({ className, role }: IProject) => {
   return (
     <div className={cn("", className)}>
       <Sidebar>
-        <h2 className="text-2xl font-bold capitalize">Projects</h2>
+        <h2 className="text-2xl font-bold capitalize mb-2">Projects</h2>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {projects.map((project: any, _i: any) => (
+            <StatsCard
+              name={project.name}
+              completedTasks={project.completedTasks}
+              totalTasks={project.totalTasks}
+              progressPercentage={project.progressPercentage}
+              showDropDown={true}
+            />
+          ))}
+        </div>
+
+        <Button
+          onClick={() => {
+            console.log("hello world");
+          }}
+          className="flex items-center gap-x-2 my-3"
+        >
+          <FolderPlus size={22} strokeWidth={1.2} /> Create a new project
+        </Button>
       </Sidebar>
     </div>
   );
