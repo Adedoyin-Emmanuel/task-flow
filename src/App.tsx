fa8b44da1ab3660408ba5119044d89aa848572a3
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-08-21 14:44:05 +0100

    feat: added login and signup pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,14 @@
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Routes } from "react-router-dom";
+import login from "./pages/login";
+import signup from "./pages/signup";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<>Hello world</>} />
-        <Route path="/about" element={<></>} />
-
-        <Route path="/auth">
-          <Route path="/login" element={<>Login</>} />
-          <Route path="/signup" element={<>Signup</>} />
-        </Route>
-
-        <Route path="/admin">
-          <Route path="/users" element={<>Users page</>} />
-          <Route path="/users/:userId" element={<>User Details</>} />
-          <Route path="/reports"/>
-        </Route>  
-
-
-        <Route path="/manager">
-          <Route path="/projects" element={<>Projects</>} />
-          <Route path="/projects/:projectId" element={<>Project Details</>} />
-          <Route path="/projects/:projectId/edit" element={<>Project Details Edit</>} />
-          <Route path="/reports" />
-          <Route path="/tas"/>
-        </Route>
-
-        <Route path="/projects">
-          <Route path="/:projectId" element={<>Project Details</>} />
-          <Route path="/:projectId/edit" element={<>Project Details Edit</>} />
-          <Route path="/:projectId/edit" element={<>Project Details Edit</>} />
-        </Route>
-
-        <Route path="/dashboard">
-          <Route path="/profile" element={<>Profile</>} />
-          <Route path="/settings" element={<>Settings</>} />
-        </Route>
-
-        <Route path="*" element={<>Not Found</>} />
+       
+  
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/login/index.tsx
@@ -0,0 +1,17 @@
+
+import React from "react";
+import {Link, useNavigate} from "react-router-dom";
+
+const Login = () => {
+
+    const navigateTo = useNavigate();
+
+    return (
+        <div>
+            <h1>Login works!</h1>
+        </div>
+    );  
+}
+
+export default Login;
+    
