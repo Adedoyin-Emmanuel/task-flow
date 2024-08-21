1e5af3f5d11cbb3f30cd96570cbf2034f2c7fa31
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-08-21 22:29:15 +0100

    feat: finished tasks page

--- src/pages/task/index.tsx
+++ src/pages/task/index.tsx
@@ -1,15 +1,114 @@
 import { cn } from "@/lib/utils";
 import Sidebar from "@/components/sidebar";
+import { ListFilter } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import TaskComponent from "@/components/task";
+import React from "react";
 
 interface ITask {
   className?: string;
 }
 
 const Task = ({ className }: ITask) => {
+  const [statusFilter, setStatusFilter] = React.useState("all");
+
+  const tasks = [
+    {
+      id: 1,
+      title: "Design homepage",
+      description:
+        "Create the initial design and layout for the website's homepage.",
+      status: "completed",
+      endDate: "2024-08-18",
+    },
+    {
+      id: 2,
+      title: "Develop login feature",
+      description:
+        "Implement the login functionality, including user authentication and form validation.",
+      status: "in progress",
+      endDate: "2024-08-22",
+    },
+    {
+      id: 3,
+      title: "Fix bugs in dashboard",
+      description:
+        "Resolve existing bugs in the dashboard interface and improve performance.",
+      status: "overdue",
+      endDate: "2024-08-15",
+    },
+    {
+      id: 4,
+      title: "Fix bugs in API",
+      description:
+        "Identify and fix bugs in the API to ensure smooth data retrieval and interaction.",
+      status: "pending",
+      endDate: "2024-08-15",
+    },
+  ];
+
+  const filteredTasks = tasks.filter((task) => {
+    if (statusFilter === "all") return tasks;
+    return task.status === statusFilter;
+  });
+
   return (
     <div className={cn("", className)}>
       <Sidebar>
-        <h2 className="text-2xl font-bold capitalize">Tasks</h2>
+        <h1 className="text-2xl font-bold mb-4 capitalize flex items-center gap-x-2">
+          Tasks
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <ListFilter
+                strokeWidth={2}
+                size={23}
+                className="cursor-pointer
+          "
+              />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-56">
+              <DropdownMenuLabel>Task Status</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuRadioGroup
+                value={statusFilter}
+                onValueChange={setStatusFilter}
+              >
+                <DropdownMenuRadioItem value="pending">
+                  Pending
+                </DropdownMenuRadioItem>
+                <DropdownMenuRadioItem value="completed">
+                  Completed
+                </DropdownMenuRadioItem>
+                <DropdownMenuRadioItem value="in progress">
+                  In Progress
+                </DropdownMenuRadioItem>
+                <DropdownMenuRadioItem value="overdue">
+                  Overdue
+                </DropdownMenuRadioItem>
+              </DropdownMenuRadioGroup>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </h1>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 w-full gap-4">
+          {filteredTasks.map((task, _i) => (
+            <TaskComponent
+              key={_i}
+              title={task.title}
+              status={task.status as any}
+              endDate={task.endDate}
+              description={task.description}
+            />
+          ))}
+        </div>
       </Sidebar>
     </div>
   );
