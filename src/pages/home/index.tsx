11e7fe89e90646031f7bf6a29c40d3c149bf64c8
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-08-21 16:40:08 +0100

    feat: finished landing page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,17 +1,23 @@
-
-import React from "react";
-import {Link, useNavigate} from "react-router-dom";
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router-dom";
 
 const Home = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen ">
+      <div className="text-center p-6 ">
+        <h1 className="text-4xl font-bold mb-4 text-gray-800">
+          Welcome to Task Flow
+        </h1>
+        <p className="text-lg text-gray-600 mb-6">
+          Your personal task management solution.
+        </p>
 
-    const navigateTo = useNavigate();
-
-    return (
-        <div>
-            <h1>Home works!</h1>
-        </div>
-    );  
-}
+        <Link to={"/auth/signup"}>
+          <Button className="w-44">Get Started</Button>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
-    
--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,17 +1,10 @@
 
-import React from "react";
-import {Link, useNavigate} from "react-router-dom";
-
 const Signup = () => {
-
-    const navigateTo = useNavigate();
-
-    return (
-        <div>
-            <h1>Signup works!</h1>
-        </div>
-    );  
-}
+  return (
+    <div>
+      <h1>Signup works!</h1>
+    </div>
+  );
+};
 
 export default Signup;
-    
