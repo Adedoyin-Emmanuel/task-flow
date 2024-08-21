c030723218b2b1c7abd3d6390969410ea406604a
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-08-21 16:23:55 +0100

    feat: added project manager route

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { publicRoutes } from "@/routes/public";
 import { authRoutes } from "@/routes/auth";
 import { userRoutes } from "@/routes/user";
 import { adminRoutes } from "@/routes/admin";
+import { projectManagerRoutes } from "./routes/manager";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/auth">{authRoutes}</Route>
         <Route path="/">{userRoutes}</Route>
         <Route path="/admin">{adminRoutes}</Route>
+        <Route path="/manager">{projectManagerRoutes}</Route>
       </Routes>
     </BrowserRouter>
   );
