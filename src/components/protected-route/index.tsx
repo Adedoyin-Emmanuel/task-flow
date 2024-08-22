4d6b0c71c4ce067c31753f425c0cea25893bfebe
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-08-22 01:00:03 +0100

    feat: added protected-route

---
+++ src/components/protected-route/index.tsx
@@ -0,0 +1,18 @@
+
+import React from "react";
+
+interface ProtectedRouteProps {
+    
+}
+
+const ProtectedRoute = () => {
+
+    return (
+        <div>
+            <h1>ProtectedRoute works!</h1>
+        </div>
+    );  
+}
+
+export default ProtectedRoute;
+    
