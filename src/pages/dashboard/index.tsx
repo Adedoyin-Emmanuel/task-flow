3055256d04ac6b596ce922160d94c59d51379373
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-08-22 03:40:49 +0100

    feat: added welcome text

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -132,7 +132,9 @@ const Dashboard = ({ className }: IDashboard) => {
   return (
     <div className={cn("", className)}>
       <Sidebar>
-        <h1 className="text-2xl font-bold mb-4 capitalize">{role} Stats</h1>
+        <h1 className="text-[1.2rem] font-bold mb-4 capitalize">
+          Hi {user.name} 👋
+        </h1>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {projects.map((project: any, _i: any) => (
