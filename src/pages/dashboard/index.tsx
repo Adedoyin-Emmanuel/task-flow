6e88944a4e87c7907c0130a63dcbf3c0b4e417d6
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-08-22 06:36:35 +0100

    feat: implemeted task filtering and sortig

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -24,69 +24,25 @@ interface IDashboard {
 }
 
 const Dashboard = ({ className }: IDashboard) => {
-  let projects: any = [];
   const [statusFilter, setStatusFilter] = React.useState("all");
   const { user } = useAuth();
   const [data, setData] = React.useState([]);
+  const [allTasks, setAllTasks] = React.useState([]);
   const [loading, setLoading] = React.useState(false);
 
   if (!user) {
     return <Navigate to="/auth/login" replace />;
   }
-
-  const { role } = user;
-
-  const adminProjects = [
-    {
-      id: 1,
-      name: "Project Alpha",
-      completedTasks: 5,
-      totalTasks: 10,
-      progressPercentage: 50,
-    },
-    {
-      id: 2,
-      name: "Project Beta",
-      completedTasks: 8,
-      totalTasks: 20,
-      progressPercentage: 40,
-    },
-    {
-      id: 3,
-      name: "Project Gamma",
-      completedTasks: 15,
-      totalTasks: 15,
-      progressPercentage: 100,
-    },
-  ];
-
-  const projectManagerProjects = [
-    {
-      id: 1,
-      name: "Project Alpha",
-      completedTasks: 5,
-      totalTasks: 10,
-      progressPercentage: 50,
-    },
-  ];
-
-  const teamMemberProjects = [
-    {
-      id: 1,
-      name: "Project Alpha",
-      completedTasks: 2,
-      totalTasks: 10,
-      progressPercentage: 20,
-    },
-  ];
-
   React.useEffect(() => {
     const fetchData = async () => {
       try {
         setLoading(true);
         const response = await Axios.get("/api/dashboard/overview");
+        const response2 = await Axios.get("/api/task/");
 
-        console.log(response.data.data[0]);
+        setAllTasks(response2.data.data);
+
+        console.log(response2.data.data);
         setData(response.data.data);
       } catch (error: any) {
         toast.error(error.response?.data.message || error.message);
@@ -98,57 +54,8 @@ const Dashboard = ({ className }: IDashboard) => {
     fetchData();
   }, []);
 
-  switch (role) {
-    case "admin":
-      projects = adminProjects;
-      break;
-    case "project manager":
-      projects = projectManagerProjects;
-      break;
-    case "team member":
-      projects = teamMemberProjects;
-      break;
-    default:
-      throw new Error("Invalid role specified");
-  }
-
-  const tasks = [
-    {
-      id: 1,
-      title: "Design homepage",
-      description:
-        "Create the initial design and layout for the website's homepage.",
-      status: "completed",
-      endDate: "2024-08-18",
-    },
-    {
-      id: 2,
-      title: "Develop login feature",
-      description:
-        "Implement the login functionality, including user authentication and form validation.",
-      status: "in progress",
-      endDate: "2024-08-22",
-    },
-    {
-      id: 3,
-      title: "Fix bugs in dashboard",
-      description:
-        "Resolve existing bugs in the dashboard interface and improve performance.",
-      status: "overdue",
-      endDate: "2024-08-15",
-    },
-    {
-      id: 4,
-      title: "Fix bugs in API",
-      description:
-        "Identify and fix bugs in the API to ensure smooth data retrieval and interaction.",
-      status: "pending",
-      endDate: "2024-08-15",
-    },
-  ];
-
-  const filteredTasks = tasks.filter((task) => {
-    if (statusFilter === "all") return tasks;
+  const filteredTasks = allTasks.filter((task: any) => {
+    if (statusFilter === "all") return allTasks;
     return task.status === statusFilter;
   });
 
@@ -209,15 +116,19 @@ const Dashboard = ({ className }: IDashboard) => {
         </h1>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 w-full gap-4">
-          {filteredTasks.map((task, _i) => (
-            <Task
-              key={_i}
-              title={task.title}
-              status={task.status as any}
-              endDate={task.endDate}
-              description={task.description}
-            />
-          ))}
+          {filteredTasks.length > 0 ? (
+            filteredTasks.map((task: any, _i) => (
+              <Task
+                key={_i}
+                title={task.name}
+                status={task.status as any}
+                endDate={task.end_date}
+                description={task.description}
+              />
+            ))
+          ) : (
+            <p className="text-sm capitalize">No tasks found</p>
+          )}
         </div>
       </Sidebar>
     </div>
