8dbd05d956fe12ddb7328815c9eb36663632224b
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-08-22 03:06:46 +0100

    feat: finished login auth page

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -62,10 +62,8 @@ const Signup = () => {
       };
 
       await Axios.get("/sanctum/csrf-cookie");
-
       const response = await Axios.post("/api/auth/register", dataToSend);
-
-      console.log(response.data);
+      toast.success(response.data.message);
     } catch (error: any) {
       console.log(error);
       toast.error(error.response?.data.message || error.message);
